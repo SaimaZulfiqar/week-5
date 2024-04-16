8e4376498031a07e6198618ab002d65565954706
Saima Zulfiqar <samzak101@yahoo.com>
2024-04-16 20:19:29 +0200

    My first button creation

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.createElement("button");
+document.body.appendChild(button);
+button.innerText = "click on me";
+button.id= "myButton";
+button.style = "width: 100px;height: 50px; backgroung-color: green;";
+
+//function buttonclicked()
+//button.style ="width: 100px; height: 50px; 
+//background-color:red; color: white"
